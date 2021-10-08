0a5910a1f2c1c7cdade4870aafbf5670f8d51354
stretinsky <90929574+stretinsky@users.noreply.github.com>
2021-10-08 23:35:14 +0300

    Add files via upload

---
+++ classWork_02.10.21/Program.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classWork_02._10._21
+{
+    class Program
+    {
+        static void SolveEqation(double a, double b, double c)
+        {
+            if (a == 0)
+            {
+                Console.WriteLine("x = {0}", (double)((-1) * c / b));
+            }
+            else
+            {
+                double discriminant = b * b - 4 * a * c;
+                if (discriminant < 0)
+                {
+                    Console.WriteLine("Корней нет");
+                }
+                else if (discriminant == 0)
+                {
+                    Console.WriteLine("х = {0}", (double)((-1) * b / (2 * a)));
+                }
+                else
+                {
+                    Console.WriteLine("x1 = {0}\nx2 = {1}", (double)((-1) * b + Math.Sqrt(discriminant)) / (2 * a), (double)((-1) * b - Math.Sqrt(discriminant)) / (2 * a));
+                }
+            }
+        }
+        static int Method(ref long multiplication, out double average, params int[] array)
+        {
+            multiplication = 1;
+            for (int i = 0; i < array.Length; i++)
+            {
+                multiplication *= array[i];
+            }
+            average = array.Average();
+            return array.Sum();
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Task 1. Решение квадратного уравнения\nВведите а");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите b");
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите c");
+            double c = double.Parse(Console.ReadLine());
+            SolveEqation(a, b, c);
+
+            Console.WriteLine("Task 2. Меняем два элемента массива местами\nДан массив: ");
+            Random r = new Random();
+            int[] array = new int[20];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = r.Next(1000);
+                Console.Write("{0}: {1}; ", i + 1, array[i]);
+            }
+            Console.WriteLine("\nВведите первый индекс элемента, который нужно поменять");
+            byte index_one = byte.Parse(Console.ReadLine());
+            if (index_one <= 0 | index_one > 19)
+            {
+                Console.WriteLine("Выход за границы массива, первый индекс будет равен 1 теперь");
+                index_one = 1;
+            }
+            Console.WriteLine("Введите второй элемента, который нужно поменять");
+            byte index_two = byte.Parse(Console.ReadLine());
+            if (index_two <= 0 | index_two > 19)
+            {
+                Console.WriteLine("Выход за границы массива, второй индекс будет равен 1 теперь");
+                index_two = 1;
+            }
+            int temp = array[index_one - 1];
+            array[index_one - 1] = array[index_two - 1];
+            array[index_two - 1] = temp;
+            Console.WriteLine("Получившийся массив:");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write("{0}: {1}; ", i + 1, array[i]);
+            }
+
+            Console.WriteLine("\nTask 3. Cортировка массива из задания 2 пузырьком");
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[j] < array[i])
+                    {
+                        temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("Получившийся массив");
+            Console.WriteLine(String.Join(", ", array));
+
+            Console.WriteLine("Task 4\nДан массив");
+            long mult = 0;
+            double avg;
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = r.Next(20);
+                Console.Write("{0} ", array[i]);
+            }
+            Console.WriteLine("\nСумма элементов массива равна {0}\nПроизведение элементов равно равно {1}\nСреднее значение элементов массива равно {2}", Method(ref mult, out avg, array), mult, avg);
+
+            Console.WriteLine("Task 5");
+            try
+            {
+                while (true)
+                {
+                    Console.ResetColor();
+                    Console.Clear();
+                    Console.WriteLine("Введите число от 0 до 9. Введите 'exit' или 'закрыть', если хотите завершить работу программы");
+                    string input = Console.ReadLine().ToLower();
+                    if (input == "exit" | input == "закрыть")
+                    {
+                        Console.WriteLine("bye");
+                        break;
+                    }
+                    byte number = byte.Parse(input);
+                    if (number < 0 | number > 9)
+                    {
+                        Console.Clear();
+                        Task.Delay(3000);
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.WriteLine("ERROR");
+                        System.Threading.Thread.Sleep(3000);                    
+                    }
+                    else if (number == 0)
+                    {
+                        Console.Clear();
+                        Console.WriteLine(" #### \n#    #\n#    #\n#    #\n ####");
+                        Console.ReadKey();
+                    }
+                    else if (number == 1)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("  #\n###\n  #\n  #\n####");
+                        Console.ReadKey();
+                    }
+                    else if (number == 2)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("#####\n    #\n ####\n#     \n#####");
+                        Console.ReadKey();
+                    }
+                    else if (number == 3)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("#####\n   #\n ####\n     #\n#####");
+                        Console.ReadKey();
+                    }
+                    else if (number == 4)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("#   #\n#   #\n#   #\n#####\n    #\n    #");
+                        Console.ReadKey();
+                    }
+                    else if (number == 5)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("######\n#\n#####\n     #\n     #\n#####");
+                        Console.ReadKey();
+                    }
+                    else if (number == 6)
+                    {
+                        Console.Clear();
+                        Console.WriteLine(" #####\n#\n #####\n#     #\n#     #\n #####");
+                        Console.ReadKey();
+                    }
+                    else if (number == 7)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("#####\n   #\n  #\n  #");
+                        Console.ReadKey();
+                    }
+                    else if (number == 8)
+                    {
+                        Console.Clear();
+                        Console.WriteLine(" ####\n#    #\n#    #\n ####\n#    #\n#    #\n ####");
+                        Console.ReadKey();
+                    }
+                    else if (number == 9)
+                    {
+                        Console.Clear();
+                        Console.WriteLine(" ####\n#    #\n#    #\n ####\n     #\n     #\n#####");
+                        Console.ReadKey();
+                    }
+
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Вы ввели не цифру");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Слишком больше число");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ laba_02.10.21/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba_02._10._21
+{
+    class Program
+    {
+        static double Max(double numberOne, double numberTwo)
+        {
+            if (numberOne >= numberTwo)
+            {
+                return numberOne;
+            }
+            else
+            {
+                return numberTwo;
+            }
+        }
+        static void Swap(ref string lineOne, ref string lineTwo)
+        {
+            string temp = lineOne;
+            lineOne = lineTwo;
+            lineTwo = temp;
+        }
+        static bool Fact(int number, out long result)
+        {
+            result = 1;
+            try
+            {
+                checked
+                {
+                    for (int i = 1; i < number; i++)
+                    {
+                        result *= i;
+                    }
+                }
+            }
+            catch (OverflowException)
+            {
+                result = 0;
+                return false;
+            }
+            return true;
+        }
+        static long RecursFact(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            else if (n > 0)
+            {
+                return n * RecursFact(n - 1);
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Упражнение 5.1. Введите первое число");
+            double numberOne = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            double numberTwo = double.Parse(Console.ReadLine());
+            Console.WriteLine("Большее из них {0}", Max(numberOne, numberTwo));
+
+            Console.WriteLine("Упражнение 5.2. Введите первый параметр");
+            string lineOne = Console.ReadLine();
+            Console.WriteLine("Введите второй параметр");
+            string lineTwo = Console.ReadLine();
+            Swap(ref lineOne, ref lineTwo);
+            Console.WriteLine("Теперь первый параметр это '{0}', а второй это '{1}'", lineOne, lineTwo);
+
+            Console.WriteLine("Упражнение 5.3. Введите число");
+            int input = int.Parse(Console.ReadLine());
+            long result;
+            Console.WriteLine(Fact(input, out result));
+            Console.WriteLine("Его факториал равен {0}", result);
+
+            Console.WriteLine("Упражнение 5.4. Введите число");
+            Console.WriteLine("Его факториал равен {0}", RecursFact(int.Parse(Console.ReadLine())));
+            Console.ReadKey();
+        }
+    }
+}
